ac3b4c8444ef1e2f4935f8a6f30e0a5d28a06c53
almostdevelopr <minhalam.dev@gmail.com>
2022-08-22 13:18:15 +0530

    validate serializer

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -9,3 +9,14 @@ class PersonSerializer(serializers.ModelSerializer):
         model = Person
         # exclude = ['name']
         fields = "__all__"
+
+    def validate(self, data):
+        speacial_characters = "!@#$%^&*()_+?_+,<>/"
+        if any(c in speacial_characters for c in data['name']):
+            raise serializers.ValidationError(
+                "name cannot contain special characters")
+
+        if data['age'] < 18:
+            raise serializers.ValidationError("age should be greater than 18")
+
+        return data
